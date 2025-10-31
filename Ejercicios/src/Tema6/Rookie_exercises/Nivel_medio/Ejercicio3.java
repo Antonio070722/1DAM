06506ee680973950e20602b983068594855d1178
PC-ANTONIO-CASA <antoniollorentepalomo@gmail.com>
2025-11-01 00:51:35 +0100

    Ejercicio3.java del paquete rookie exercises hecho

---
+++ Ejercicios/src/Tema6/Rookie_exercises/Nivel_medio/Ejercicio3.java
@@ -0,0 +1,21 @@
+package Tema6.Rookie_exercises.Nivel_medio;
+
+public class Ejercicio3 {
+//    Crea un ejercicio que imprima en pantalla todas las tablas de multiplicar desde el 1 hasta
+//    el 15. Debes utilizar únicamente bucles de tipo "do...while".
+    public static void main(String[] args) {
+        int inicio = 1;
+        int finnums = 10;
+        int fintablas = 15;
+        int i=1;
+        do {
+            do {
+                System.out.println(inicio + " * " + i + " = " + inicio * i);
+                i++;
+            } while (i <= finnums);
+            System.out.println(" ");
+            i=0;
+            inicio++;
+        }while (inicio <= fintablas);
+    }
+}
