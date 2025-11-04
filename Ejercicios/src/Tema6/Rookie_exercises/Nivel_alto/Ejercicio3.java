b4642b941bb62909aa091756643b9fe50c86fa3d
PC-ANTONIO-CASA <antoniollorentepalomo@gmail.com>
2025-11-04 23:15:36 +0100

    Ejericcio3.java nivel alto rookie exercises hecho por fín.

--- Ejercicios/src/Tema6/Rookie_exercises/Nivel_alto/Ejercicio3.java
+++ Ejercicios/src/Tema6/Rookie_exercises/Nivel_alto/Ejercicio3.java
@@ -7,23 +7,29 @@ public class Ejercicio3 {
     //dado.
     static void main() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dime de qué número quieres que muestre si es perfecto: ");
-        int limite = sc.nextInt();
-        numerosPerfectosHastaLimite(limite);
+        System.out.println("Dime desde qué número quieres que se muestre los perfectos: ");
+        int inicio = sc.nextInt();
+        System.out.println("Dime hasta qué número quieres que se muestren los perfectos comprendidos: ");
+        int fin = sc.nextInt();
+        for (int a = inicio; a <= fin ; a++) {
+            esPerfectoONo(a);
+        }
+
+
     }
 
-    private static void numerosPerfectosHastaLimite(int limite) {
+    private static void esPerfectoONo(int limite) {
         int sumadivisores=0;
         for (int i = 1; i <= limite; i++) {
             if (limite%i == 0) {
-                System.out.println(i);
+                //System.out.println(i);
                 sumadivisores=sumadivisores+i;
             } else {
             }
         }
         sumadivisores=sumadivisores-limite;
-        System.out.println(" ");
-        System.out.println(sumadivisores);
+        //System.out.println(" ");
+        //System.out.println(sumadivisores);
         if (sumadivisores==limite){
             System.out.println("Es perfecto");
         } else {
