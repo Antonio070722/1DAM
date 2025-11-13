a5fb3febdc2680cd45b7be3e36de386bc322b531
PC-ANTONIO-CASA <antoniollorentepalomo@gmail.com>
2025-11-13 02:48:42 +0100

    Hundir_flota_1d.java mas o menos hecho y comentados los metodos hechos por mí

--- Ejercicios/src/Repaso_Examenes/Hundir_flota_1d.java
+++ Ejercicios/src/Repaso_Examenes/Hundir_flota_1d.java
@@ -12,31 +12,74 @@ public class Hundir_flota_1d {
         ponerBarco(tablero);
         mostrarTablero(tablero);
         ataque(tablero);
-        mostrarTablero(tablero);
+//        mostrarTablero(tablero);
 
     }
 
+    /**
+     * En este método, se genera un número aleatorio en el rango de valores de la array del tablero, el número se valida
+     * en otro método, de forma que no se puede repetir, si el número generado coincide con que es barco (B) se imprime
+     * en pantalla y la letra B se sustituye por una X, en el caso contrario, si es agua (A), se mostrará; cuando haya
+     * tres X (debido a que le único tamaño de barcos es de tres posiciones), el programa termina mostrando un mensaje
+     * de barco hundido y el número de intentos de la máquina.
+     * @param tablero
+     */
     private static void ataque(String[] tablero) {
         int posicionAtacar = 0;
+        int intentos = 0;
+        int[] repetidos = new int[tablero.length];
+        int contadorNumsUsados=0;
         do{
-            posicionAtacar = Utils.pedirNumeroAleatorio(1, tablero.length-1);
+            do {
+                posicionAtacar = Utils.pedirNumeroAleatorio(1, tablero.length-1);
+            }while (esRepetido(posicionAtacar, repetidos, contadorNumsUsados));
+            repetidos[contadorNumsUsados] = posicionAtacar;
+            contadorNumsUsados++;
+
             System.out.println("Posición Atacar: "+posicionAtacar);
             if (tablero[posicionAtacar].equals("A")){
                 System.out.println("AGUA");
+                mostrarTablero(tablero);
             }
 
             if (tablero[posicionAtacar].equals("B")){
                 System.out.println("BOOOOOM!!! BARCO");
                 tablero[posicionAtacar]="X";
+                mostrarTablero(tablero);
             }
+            intentos++;
         }
-        while(!hundido(tablero, posicionAtacar));
+        while(!hundido(tablero));
 
-        System.out.println("BARCO HUNDIDO!!!!!");
-
+        System.out.println("\n\nBARCO HUNDIDO!!!!!");
+        System.out.println("Intentos: "+intentos);
     }
 
-    private static boolean hundido(String[] tablero, int posicionAtacar) {
+    /**
+     * Este método lo he creado para validar el número generado por la máquina para intentar dar al barco
+     * @param posicionAtacar este es el número generado por la máquina
+     * @param repetidos en esta arrays van siendo almacenados los números creados por la máquina para compararlos
+     * @param contadorNumsUsados Este es un contador para almacenar en cada hueco el número generado de forma
+     *                           sucesiva en la array
+     * @return
+     */
+    private static boolean esRepetido(int posicionAtacar, int[] repetidos, int contadorNumsUsados) {
+        for (int i = 0; i < repetidos.length; i++) {
+            if (repetidos[i] == posicionAtacar) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Este es el método en el que se comprueba si hay 3 X, es decir tres aciertos a barcos, lo que implica que termina
+     * el programa.
+     * @param tablero Valor que se contiene en cada valor de la Array tablero, el bucle for recorre cada uno de estos
+     *                y, si es X se suma al contador, que cuando sea 3 el total de hundidos devuelve true.
+     * @return
+     */
+    private static boolean hundido(String[] tablero) {
         boolean hundido = false;
         int totalHundidos=0;
         for (int i = 0; i < tablero.length; i++) {
