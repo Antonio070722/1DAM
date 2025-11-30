76a44dc806339228df59d33bd657f79d981a1ae4
PC-ANTONIO-CASA <antoniollorentepalomo@gmail.com>
2025-11-30 12:59:43 +0100

    sorthim algorithm hecho

--- Ejercicios/src/Tema8/SortProPlus/Ejercicio1.java
+++ Ejercicios/src/Tema8/SortProPlus/Ejercicio1.java
@@ -1,5 +1,7 @@
 package Tema8.SortProPlus;
 
+import Utilidades.Utils;
+
 public class Ejercicio1 {
 
     /**
@@ -9,30 +11,47 @@ public class Ejercicio1 {
      * siguiente enlace http://lwh.free.fr/pages/algo/tri/tri_bulle_es.html
      */
 
-    static void main(String[] args) {
-        /**
-         * Método del algoritmo de organización:
-         * void bubble_sort(int* vector)
-         * {
-         *     int iteración = 0;
-         *     bool permutation = true;
-         *     int actual;
-         *
-         *     while ( permutation) {
-         *         permutation = false;
-         *         iteración ++;
-         *         for (actual=0;actual<20-iteración;actual++) {
-         *             if (vector[actual]>vector[actual+1]){
-         *                 permutation = true;
-         *                 // Intercambiamos los dos elementos
-         *                 int temp = vector[actual];
-         *                 vector[actual] = vector[actual+1];
-         *                 vector[actual+1] = temp;
-         *             }
-         *         }
-         *     }
-         * }
-         */
+    public static void main(String[] args) {
+        int tamano = 20; // número de elementos, puedes cambiarlo
+        int[] datos = new int[tamano];
+
+        // Llenar el array con valores aleatorios entre 0 y 1000
+        for (int i = 0; i < datos.length; i++) {
+            datos[i] = Utils.pedirNumeroAleatorio(0, 1000);
+        }
+
+        // Ordenar
+        int[] ordenado = bubbleSort(datos);
+
+        // Imprimir resultado
+        for (int num : ordenado) {
+            System.out.print(num + " ");
+        }
     }
 
+    public static int[] bubbleSort(int[] vector) {
+        boolean permutation = true;
+        int iteracion = 0;
+
+        while (permutation) {
+            permutation = false;
+            iteracion++;
+
+            for (int actual = 0; actual < vector.length - iteracion; actual++) {
+                if (vector[actual] > vector[actual + 1]) {
+                    // Intercambiamos los dos elementos
+                    int temp = vector[actual];
+                    vector[actual] = vector[actual + 1];
+                    vector[actual + 1] = temp;
+
+                    permutation = true;
+                }
+            }
+        }
+        return vector;
+    }
+
+
 }
+
+
