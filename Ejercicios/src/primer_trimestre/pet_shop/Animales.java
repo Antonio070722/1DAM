4270274d1fb110d086bc70fc8357b963d90c47f0
Antonio Llorente <allopal2911@g.educaand.es>
2025-12-11 16:25:00 +0100

    Validación de edad en Animales.java

--- Ejercicios/src/primer_trimestre/pet_shop/Animales.java
+++ Ejercicios/src/primer_trimestre/pet_shop/Animales.java
@@ -11,4 +11,45 @@ public class Animales {
         this.EAN=EAN;
     }
 
+    public SEXO getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(SEXO sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad<0){
+            throw new IllegalArgumentException("La edad no puede ser negativa");
+        }
+        if (edad > 36500){
+            throw new IllegalArgumentException("Edad no válida: "+edad+" días.");
+        }
+        this.edad = edad;
+    }
+
+    public String getEAN() {
+        return EAN;
+    }
+
+    public void setEAN(String EAN) {
+        this.EAN = EAN;
+    }
+
+    @Override
+    public String toString() {
+        return "Animales{" +
+                "sexo=" + sexo +
+                ", edad=" + edad +
+                ", EAN='" + EAN + '\'' +
+                '}';
+    }
+
+
+
 }
